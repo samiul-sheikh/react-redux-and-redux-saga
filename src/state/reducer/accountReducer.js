f852b1c73e5c41b206f8f158d60fae29f3499a50
samiul-sheikh <sheikh.samiul1971@gmail.com>
2022-01-23 23:19:00 +0600

    make accountreducer

--- src/state/reducer/accountReducer.js
+++ src/state/reducer/accountReducer.js
@@ -1,3 +1,10 @@
-const reducer = (state) => {
-    
-}
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case "deposit":
+            return state + action.payload;
+        case "withdraw":
+            return state - action.payload;
+        default:
+            return state
+    }
+};
